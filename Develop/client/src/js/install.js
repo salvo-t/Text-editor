73b7a48270153abc7185373efa5f6675f22e8626
salvo-t <salvot.biz@gmail.com>
2023-02-25 22:19:57 -0500

    added a handler for appinstalled event for install.js

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -24,5 +24,5 @@ butInstall.addEventListener('click', async () => {
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-
+    window.deferredPrompt = null;
 });
