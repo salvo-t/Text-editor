e1ba230dafb4628c4c6ac29193709412a8ee7500
salvo-t <salvot.biz@gmail.com>
2023-02-27 21:46:09 -0500

    finished project

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.use(express.static('../client/dist'));
+const DIST_PATH = path.resolve('../client/dist')
+app.use(express.static(DIST_PATH));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
